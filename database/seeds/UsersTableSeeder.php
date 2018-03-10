d1602f687af0e9ea067a68ad4117a5a51ceed17a
mum hrtest <mumhrtest@vistaprint.net>
2018-03-10 15:21:18 +0530

    Blog Post project

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'krunal admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+			'role' => 'admin',
+        ]);
+    }
+}
---
+++ public/assets/js/custom.js
@@ -0,0 +1,4 @@
+function cancelPostCreate()
+{
+	window.history.back();
+}
